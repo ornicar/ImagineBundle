031b8b525369d3756fe0126e47e4933f1b4c44c0
Matthieu Bontemps <matthieu@knplabs.com>
2010-09-20 03:33:21 +0800

    Update to the latest Symfony2 (removed Symfony\Framework)

--- ImagineBundle.php
+++ ImagineBundle.php
@@ -2,7 +2,7 @@
 
 namespace Bundle\ImagineBundle;
 
-use Symfony\Framework\Bundle\Bundle as BaseBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 
 class ImagineBundle extends BaseBundle
 {
