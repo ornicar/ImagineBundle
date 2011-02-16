ee4ba94cb2f20c00b33e76f330927f7d3cc7dfbc
ornicar <thibault.duplessis@gmail.com>
2011-02-16 14:36:57 -0800

    Update DIC extension

--- ImagineBundle.php
+++ ImagineBundle.php
@@ -8,9 +8,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 
 class ImagineBundle extends BaseBundle
 {
-    public function registerExtensions(ContainerBuilder $container)
+    public function build(ContainerBuilder $container)
     {
-        parent::registerExtensions($container);
+        parent::build($container);
         $container->addCompilerPass(new ImaginePass());
     }
 
